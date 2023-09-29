3c51637355c48ca16c7d00508209fc2f1ca1af68
Vincent Dirks <vincent@dirksonline.net>
2023-09-30 12:12:48 +1300

    initial commit from Spring Initializr

---
+++ src/test/java/net/dirksonline/securiteasetechassessment/SecuritEaseTechAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dirksonline.securiteasetechassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritEaseTechAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
