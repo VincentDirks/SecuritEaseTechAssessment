a3ee63da5036a9848f3b3b1a85796306bb10ff8f
Vincent Dirks <vincent@dirksonline.net>
2023-09-30 21:52:32 +1300

    Add a check for the George user record Add @Builder annotation for User class Create the lombok.config file

--- src/test/java/net/dirksonline/securiteasetechassessment/APITest.java
+++ src/test/java/net/dirksonline/securiteasetechassessment/APITest.java
@@ -17,7 +17,7 @@ public class APITest {
         RestAssured.baseURI = baseURI;
     }
 
-    @Test
+    @Test(description = "Fetch first page of list of users, and it contains a user George")
     public void testGetUsers() {
         Response response = given()
                 .when()
@@ -25,23 +25,39 @@ public class APITest {
                 .then()
                 .extract().response();
 
-        // Validate the response status code
+        // Verify the response status code
         assertThat(response.getStatusCode())
                 .as("Response Status Code")
                 .isEqualTo(SC_OK);
 
-        // Validate the response content type
+        // Verify the response content type
         assertThat(response.getContentType())
                 .as("Response Content Type")
                 .contains(ContentType.JSON.toString());
 
+        // Verify the response schema
         UserListPage userListPage = response.as(UserListPage.class);
-
         assertThat(userListPage)
                 .as("Response contains a valid userListPage response object")
                 .isInstanceOf(UserListPage.class);
 
-        // Validate the response schema (example: user_id, username, email)
+        // Verify the "George" record is returned
+        UserListPage.User user = UserListPage.User.builder()
+                .id(1)
+                .first_name("George")
+                .last_name("Bluth")
+                .email("george.bluth@reqres.in")
+                .avatar("https://reqres.in/img/faces/1-image.jpg")
+                .build();
+
+        assertThat(userListPage.data.stream()
+                .filter( u -> u.first_name.equals("George"))
+                .findAny()
+                .orElseThrow()
+        )
+                .as("Response contains George user")
+                .isEqualTo(user);
+
         // Example assertion:
         // assertNotNull(response.jsonPath().getString("user_id"), "user_id not found in response");
     }
