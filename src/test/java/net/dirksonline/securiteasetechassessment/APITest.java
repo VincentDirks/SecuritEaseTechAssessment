e7b99f7e0868f8be518688d72b8893f75422a07d
Vincent Dirks <vincent@dirksonline.net>
2023-10-01 15:33:09 +1300

    Adding explanatory comments. Updated README

--- src/test/java/net/dirksonline/securiteasetechassessment/APITest.java
+++ src/test/java/net/dirksonline/securiteasetechassessment/APITest.java
@@ -10,20 +10,28 @@ import static org.apache.http.HttpStatus.SC_OK;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class APITest {
+    /**
+     * Set the prefix for all API calls. It's a private property in the APITest class.
+     * It is not accessible in other classes, and can not be configured at run time for different environments.
+     * It would be best if it was moved to an external property file, and accessed through a shared configuration object.
+     */
     private final String baseURI = "https://reqres.in/api";
 
+    /** Setup RestAssured, just the baseURI for now */
     @BeforeClass
     public void setup() {
         RestAssured.baseURI = baseURI;
     }
 
+    /**
+     * A testng scenario annotated with a description
+     */
     @Test(description = "Fetch first page of list of users, and it contains a user George")
     public void testGetUsers() {
-        Response response = given()
-                .when()
-                .get("/users")
-                .then()
-                .extract().response();
+        Response response =
+                given()                       // a place to setup initial conditions for the REST call
+                .when().get("/users")         // the action to perform, eg. GET the /users endpoint
+                .then().extract().response(); // fetches the response object
 
         // Verify the response status code
         assertThat(response.getStatusCode())
@@ -35,13 +43,13 @@ public class APITest {
                 .as("Response Content Type")
                 .contains(ContentType.JSON.toString());
 
-        // Verify the response schema
+        // Verify the response schema using the UserListPage pojo
         UserListPage userListPage = response.as(UserListPage.class);
         assertThat(userListPage)
                 .as("Response contains a valid userListPage response object")
                 .isInstanceOf(UserListPage.class);
 
-        // Verify the "George" record is returned
+        // build a User object for the expected George user
         UserListPage.User user = UserListPage.User.builder()
                 .id(1)
                 .first_name("George")
@@ -50,6 +58,7 @@ public class APITest {
                 .avatar("https://reqres.in/img/faces/1-image.jpg")
                 .build();
 
+        // Verify the "George" record is returned, and has the expected values
         assertThat(userListPage.data.stream()
                 .filter( u -> u.first_name.equals("George"))
                 .findAny()
@@ -61,30 +70,24 @@ public class APITest {
 
     @Test(description = "Fetch second page of list of users, and it contains another user George")
     public void testGetUsersPage2() {
-        Response response = given()
-                .param("page",2)
-                .when()
-                .get("/users")
-                .then()
-                .extract().response();
+        Response response =
+                given().param("page",2)
+                .when().get("/users")
+                .then().extract().response();
 
-        // Verify the response status code
         assertThat(response.getStatusCode())
                 .as("Response Status Code")
                 .isEqualTo(SC_OK);
 
-        // Verify the response content type
         assertThat(response.getContentType())
                 .as("Response Content Type")
                 .contains(ContentType.JSON.toString());
 
-        // Verify the response schema
         UserListPage userListPage = response.as(UserListPage.class);
         assertThat(userListPage)
                 .as("Response contains a valid userListPage response object")
                 .isInstanceOf(UserListPage.class);
 
-        // Verify the "George" record is returned
         UserListPage.User user = UserListPage.User.builder()
                 .id(11)
                 .first_name("George")
@@ -101,5 +104,4 @@ public class APITest {
                 .as("Second page response contains another George user")
                 .isEqualTo(user);
     }
-
 }
